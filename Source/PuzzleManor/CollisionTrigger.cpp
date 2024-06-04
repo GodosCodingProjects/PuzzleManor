4d227561fc85e6b87741e93f65bd374c2dd30895
Godo <godoscodingprojects@gmail.com>
2024-06-03 21:31:06 -0400

    Added Collision trigger

---
+++ Source/PuzzleManor/CollisionTrigger.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollisionTrigger.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+void UCollisionTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto Collision = GetOwner()->FindComponentByClass<UBoxComponent>();
+	if (Collision)
+	{
+		Collision->OnComponentBeginOverlap.AddUniqueDynamic(this, &UCollisionTrigger::OnCollision);
+	}
+}
+
+void UCollisionTrigger::OnCollision(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor == Cast<AActor>(UGameplayStatics::GetPlayerCharacter(GetOwner()->GetWorld(), 0)))
+	{
+		Trigger();
+	}
+}
---
+++ Source/PuzzleManor/CollisionTrigger.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Trigger.h"
+#include "CollisionTrigger.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class PUZZLEMANOR_API UCollisionTrigger : public UTrigger
+{
+	GENERATED_BODY()
+	
+public:
+	void BeginPlay() override;
+
+	UFUNCTION()
+	void OnCollision(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
